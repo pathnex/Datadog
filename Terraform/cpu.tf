b5df5186fc4e02021274efc5925aa999cd913ea3
PathNex <pathnex.com@gmail.com>
2026-03-25 12:58:21 +0530

    Updated with the ec2 and Monitors

--- Terraform/cpu.tf
+++
@@ -1,6 +0,0 @@
-resource "datadog_monitor" "high_cpu" {
-  name              = "High CPU Usage"
-  type              = "metric alert"
-  query             = "avg(last_5m):avg:system.cpu.user{env:${var.env}} > 80"
-  message           = "CPU is high on {{host.name}}"
-}
